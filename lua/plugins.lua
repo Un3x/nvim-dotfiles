c34352321d50ecf27c163721af8630d8766f674b
Thomas COMES <thomas.comes.tech@gmail.com>
2024-09-06 09:38:20 +0200

    Added copilot chat

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -26,6 +26,7 @@ return {
     version = "*",
     dependencies = 'nvim-tree/nvim-web-devicons'
   },
+
   -- Vim illuminate
   "RRethy/vim-illuminate",
   --Telescope
@@ -72,5 +73,19 @@ return {
     config = function()
       require("image_preview").setup()
     end
+  },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+    },
+    build = "make tiktoken", -- Only on MacOS or Linux
+    opts = {
+      debug = true, -- Enable debugging
+      -- See Configuration section for rest
+    },
+    -- See Commands section for default commands if you want to lazy load on them
   },
 }

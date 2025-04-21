95be0ab6c53542e2e040449f2cf99f2c3823e087
Thomas COMES <thomas.comes.tech@gmail.com>
2025-04-21 09:32:00 +0200

    Enhanced config file organization

--- init.lua
+++ init.lua
@@ -1,39 +1,33 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+-- disable netrw at the very start
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- Core setup
 require("config.lazy-config")
 require("plugins")
 require("autocmd")
 require("keymaps")
 require("options")
 
-require("config.noice-config")
-require("config.bufferline-config")
-require("config.illuminate-config")
-require("config.lsp-config")
-require("config.copilot-config")
-require("config.flit-config")
-require("config.treesitter-config")
-require("config.telescope-config")
-require("config.cmp-config")
-
+-- Theme configuration
 require("config.tokyonight-config")
 vim.cmd[[colorscheme tokyonight]]
 
--- disable netrw at the very start of your init.lua
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+-- UI configurations
+require("config.noice-config")
+require("config.bufferline-config")
+require("config.nvim-tree-config")
 
--- OR setup with some options
-require("nvim-tree").setup({
-  sort_by = "case_sensitive",
-  view = {
-    width = 30,
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
-})
+-- Editor features
+require("config.illuminate-config")
+require("config.treesitter-config")
+require("config.telescope-config")
+require("config.flit-config")
+
+-- Language & completion
+require("config.lsp-config")
+require("config.cmp-config")
+require("config.copilot-config")

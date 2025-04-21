d54aafa67effde3676da18fcc428d4f3259ceb47
Thomas COMES <thomas.comes.tech@gmail.com>
2025-04-21 09:28:15 +0200

    Improved lazy loading

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -24,48 +24,94 @@ return {
   {
     'akinsho/bufferline.nvim',
     version = "*",
+    event = "BufReadPost",
     dependencies = 'nvim-tree/nvim-web-devicons'
   },
 
   -- Vim illuminate
-  "RRethy/vim-illuminate",
+  {
+    "RRethy/vim-illuminate",
+    event = "BufReadPost"
+  },
+  
   --Telescope
   {
-    'nvim-telescope/telescope.nvim', tag = '0.1.3',
+    'nvim-telescope/telescope.nvim', 
+    tag = '0.1.3',
+    cmd = "Telescope",
+    keys = {
+      { "<leader>ff", "<cmd>Telescope find_files<cr>" },
+      { "<leader>fg", "<cmd>Telescope live_grep<cr>" },
+    },
     dependencies = {
       'nvim-lua/plenary.nvim',
       'nvim-telescope/telescope-file-browser.nvim'
     }
   },
+  
   -- LSP
   {'williamboman/mason.nvim'},
   {'williamboman/mason-lspconfig.nvim'},
   {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x', lazy = true, config= false },
   {'neovim/nvim-lspconfig'},
+  
   -- Completion
-  {'hrsh7th/cmp-nvim-lsp'},
-  {'hrsh7th/nvim-cmp'},
+  {
+    'hrsh7th/cmp-nvim-lsp',
+    event = "InsertEnter"
+  },
+  {
+    'hrsh7th/nvim-cmp',
+    event = {"InsertEnter", "CmdlineEnter"}
+  },
+  
   -- Copilot
-  { "zbirenbaum/copilot.lua", },
+  { 
+    "zbirenbaum/copilot.lua",
+    event = "InsertEnter"
+  },
   {
     "zbirenbaum/copilot-cmp",
+    event = "InsertEnter",
     config = function ()
       require("copilot_cmp").setup()
     end
   },
+  
   -- Leap
-  "ggandor/leap.nvim",
-  "ggandor/flit.nvim",
+  {
+    "ggandor/leap.nvim",
+    keys = { "s", "S" },
+    dependencies = { "ggandor/flit.nvim" }
+  },
+  
   -- Tree
-  "nvim-tree/nvim-tree.lua",
+  {
+    "nvim-tree/nvim-tree.lua",
+    cmd = {"NvimTreeToggle", "NvimTreeOpen", "NvimTreeFocus"}
+  },
+  
   -- VimRails
-  "tpope/vim-rails",
+  {
+    "tpope/vim-rails",
+    ft = {"ruby", "eruby", "yaml", "haml"}
+  },
+  
   -- Treesitter
   'nvim-treesitter/nvim-treesitter',
+  
   -- Git
-  'tpope/vim-fugitive',
+  {
+    'tpope/vim-fugitive',
+    cmd = {"Git", "Gstatus", "Gblame", "Gdiff"}
+  },
+  
   -- which-key
-  "folke/which-key.nvim",
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy"
+  },
+  
   -- preview images
   {
     'https://github.com/adelarsq/image_preview.nvim',
@@ -74,16 +120,20 @@ return {
       require("image_preview").setup()
     end
   },
+  
+  -- CopilotChat
   {
     "CopilotC-Nvim/CopilotChat.nvim",
+    cmd = {
+      "CopilotChat",
+      "CopilotChatToggle",
+      "CopilotChatExplain"
+    },
     dependencies = {
-      { "github/copilot.vim" },
+      { "zbirenbaum/copilot.lua" },
       { "nvim-lua/plenary.nvim", branch = "master" },
     },
-    build = "make tiktoken", -- Only on MacOS or Linux
-    opts = {
-      -- See Configuration section for options
-    },
-    -- See Commands section for default commands if you want to lazy load on them
+    build = "make tiktoken",
+    opts = {},
   },
 }
